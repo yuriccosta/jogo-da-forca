46a41ae2ccbb14e5b5f26ed6cb0ff8ef21361e01
zzAlfa <yuricoutinhoc@gmail.com>
2023-07-21 16:38:48 -0300

    Versão 2.0

--- forca.py
+++ forca.py
@@ -1,11 +1,12 @@
-import random
-import funcoes as fc
+# Feito por zzAlfa / Versão 2.0
 
-# Feito por zzAlfa / Versão 1.0
+import random
+import time
+import funcoes as fc
 
 wins = 0
 while True:
-    boneco = {"cabeça": "('_')", "corpo": " (|)", "pernas": r" / \ "}
+    # Menu
     print('Bem vindo ao Jogo da Forca!!!')
     print('*' * 20)
     print(f'Número de vitórias: {wins}')
@@ -14,130 +15,99 @@ while True:
     (1) Frutas
     (2) Cidades
     (3) Animais
-    
+
     (0) Sair
     ''')
+
+    # Pega a escolha do jogador e valida
     while True:
         try:
             esc = int(input('Selecione o modo de jogo: '))
-            break
         except ValueError:
-            print('Tente digitar um número inteiro')
+            print('Tente digitar um número inteiro: ')
+        else:
+            break
 
+    '''
+    life = vida do jogador, oculta = palavra oculta, trys = todas as tentativas do jogador
+    points = pontos de acerto do jogador
+    '''
+    life = 4
+    oculta = []
+    trys = []
+    points = 0
+
+    # Escolha dos temas
     if esc == 1:
         fruta = ['MAÇÃ', 'BANANA', 'UVA']
         print('Bem vindo ao tema de frutas!!!')
-        play = fruta[random.randint(0, 2)]
-        trys = []
-        myword = ''
-        count = 0
-        life = 4
-        while True:
-            acert = 0
-            fc.forca(play, trys, life)
-            if myword == play:
-                print('Você ganhou o Jogo!!!')
-                wins += 1
-                break
-            elif life == 0:
-                print('Você perdeu o jogo')
-		break
-            while True:
-                letra = input('Qual palavra deseja tentar: ').upper()
-                if letra not in trys:
-                    trys.append(letra)
-                    break
-                else:
-                    print('Você está digitando a mesma letra, tente novamente')
+        palavra = fruta[random.randint(0, len(fruta) - 1)]
 
-            print('=' * 30)
-            for c in range(0, len(play)):
-                if trys[count] == play[c]:
-                    print(f'Você acertou a letra {trys[count]}')
-                    myword = myword + play[c]
-                    acert = 1
-            if acert != 1:
-                print(f'Você errou a letra {trys[count]}')
-                life = life - 1
-            count += 1
-            print('=' * 60)
     elif esc == 2:
         cidades = ['ARACAJU', 'BELO HORIZONTE', 'BOA VISTA', 'BRASÍLIA', 'CAMPO GRANDE', 'CUIABÁ', 'FLORIANÓPOLIS',
                    'FORTALEZA''GOIÂNIA', 'JOÃO PESSOA', 'MACAPÁ', 'MACEIÓ', 'MANAUS', 'NATAL', 'PALMAS', 'PORTO ALEGRE',
-                   'PORTO VELHO', 'RECIFE', 'RIO BRANCO', 'RIO DE JANEIRO', 'SALVADOR', 'SÃO LUÍS', 'SÃO PAULO', 'TERESINA',
+                   'PORTO VELHO', 'RECIFE', 'RIO BRANCO', 'RIO DE JANEIRO', 'SALVADOR', 'SÃO LUÍS', 'SÃO PAULO',
+                   'TERESINA',
                    'VITÓRIA']
         print('Bem vindo ao tema de cidades!!!')
-        play = cidades[random.randint(0, 2)]
-        trys = []
-        myword = ''
-        count = 0
-        life = 4
-        while True:
-            acert = 0
-            fc.forca(play, trys, life)
-            if myword == play:
-                print('Você ganhou o Jogo!!!')
-                wins += 1
-                break
-            elif life == 0:
-                print('Você perdeu o jogo')
-		break
-            while True:
-                letra = input('Qual palavra deseja tentar: ').upper()
-                if letra not in trys:
-                    trys.append(letra)
-                    break
-                else:
-                    print('Você está digitando a mesma letra, tente novamente')
+        palavra = cidades[random.randint(0, len(cidades) - 1)]
 
-            print('=' * 30)
-            for c in range(0, len(play)):
-                if trys[count] == play[c]:
-                    print(f'Você acertou a letra {trys[count]}')
-                    myword = myword + play[c]
-                    acert = 1
-            if acert != 1:
-                print(f'Você errou a letra {trys[count]}')
-                life = life - 1
-            count += 1
-            print('=' * 60)
     elif esc == 3:
         animais = ['TIGRE', 'LOBO', 'JACARÉ', 'ONÇA-PINTADA', 'ARARA', 'CARANGUEIJO', 'CACHORRO', 'LEÃO']
-        print('Bem vindo ao tema de cidades!!!')
-        play = animais[random.randint(0, 2)]
-        trys = []
-        myword = ''
-        count = 0
-        life = 4
-        while True:
-            acert = 0
-            fc.forca(play, trys, life)
-            if myword == play:
-                print('Você ganhou o Jogo!!!')
-                wins += 1
-                break
-            elif life == 0:
-                print('Você perdeu o jogo')
-		break
-            while True:
-                letra = input('Qual palavra deseja tentar: ').upper()
-                if letra not in trys:
-                    trys.append(letra)
-                    break
-                else:
-                    print('Você está digitando a mesma letra, tente novamente')
+        print('Bem vindo ao tema de animais!!!')
+        palavra = animais[random.randint(0, len(animais) - 1)]
 
-            print('=' * 30)
-            for c in range(0, len(play)):
-                if trys[count] == play[c]:
-                    print(f'Você acertou a letra {trys[count]}')
-                    myword = myword + play[c]
-                    acert = 1
-            if acert != 1:
-                print(f'Você errou a letra {trys[count]}')
-                life = life - 1
-            count += 1
-            print('=' * 60)
-    elif esc == 0:
+    else:
         print('Até a próxima')
         break
+
+    # Transforma a palavra sorteada em uma palavra oculta, ignorando os espaços e hifens
+    for c in palavra:
+        if c == ' ':
+            oculta.append(" ")
+            points += 1
+        elif c == '-':
+            oculta.append("-")
+            points += 1
+        else:
+            oculta.append("_")
+
+    while True:
+        fc.printforca(life, oculta, trys)
+        letra = input("Digite sua tentativa!: ").upper()
+
+        # Verifica se a tentativa está na palavra do jogo
+        while True:
+            if len(letra) > 1 or letra in " -":
+                letra = input("Digite apenas uma letra!: ").upper()
+            elif letra in trys:
+                letra = input("Você já digitou essa letra, tente outra!: ").upper()
+            else:
+                break
+
+        trys.append(letra)
+
+        # Acrescenta a letra tentada na palavra oculta e os pontos de acerto, caso esteja na palavra sorteada
+        loose = True
+        for c in range(0, len(palavra)):
+            if letra == palavra[c]:
+                oculta.pop(c)
+                oculta.insert(c, letra)
+                points += 1
+                loose = False
+
+        if loose:
+            life -= 1
+
+        # Verifica se o jogador perdeu ou se ele ganhou
+        if life == 0:
+            fc.printforca(life, oculta, trys)
+            print(f"Você perdeu! A palavra era: {palavra}")
+            time.sleep(2)
+            break
+        elif points == len(palavra):
+            wins += 1
+            fc.printforca(life, oculta, trys)
+            print(f"Você ganhou! A palavra era: {palavra}")
+            time.sleep(2)
+            break
--- funcoes.py
+++ funcoes.py
@@ -1,70 +1,18 @@
-# Feito por zzAlfa / Versão 1.0
+# Feito por zzAlfa / Versão 2.0
 
-def forca(palavra='', trys=None, points=0):
-    def letras():
-        tabel = []
-        for c in range(0, len(palavra)):
-            tabel.append('_')
-        for c in range(0, len(palavra)):
-            for p in range(0, len(trys)):
-                if palavra[c] == trys[p]:
-                    if len(tabel) == 0:
-                        tabel.append(trys[p])
-                    else:
-                        tabel.pop(c)
-                        tabel.insert(c, trys[p])
-        tabelstr = ''
-        for c in range(0, len(tabel)):
-            tabelstr = tabelstr + tabel[c] + ' '
+# Printa o corpo do boneco, a vida e a palavra do jogo
+def printforca(life=0, oculta=[], trys=[]):
+    boneco = ["('_')", " /|\\", "  |", " / \\"]
 
-        return tabelstr
-
-    if points == 4:
-        print(f'Número de vidas: {points}')
-        print(f'Número de palavras: {len(palavra)}')
-        print(f'Letras tentadas: {trys}')
-        print(fr'''{'=' * 30}
-('_')
- (|)
- / \
-/   \
-''')
-        print(letras())
-        print(f"{'=' * 30}")
-    elif points == 3:
-        print(f'Número de vidas: {points}')
-        print(f'Número de palavras: {len(palavra)}')
-        print(f'Letras tentadas: {trys}')
-        print(fr'''{'=' * 30}
-('_')
- (|)
- / \
-''')
-        print(letras())
-        print(f"{'=' * 30}")
-    elif points == 2:
-        print(f'Número de vidas: {points}')
-        print(f'Número de palavras: {len(palavra)}')
-        print(f'Letras tentadas: {trys}')
-        print(f'''{'=' * 30}
-('_')
- (|)
-''')
-        print(letras())
-        print(f"{'=' * 30}")
-    elif points == 1:
-        print(f'Número de vidas: {points}')
-        print(f'Número de palavras: {len(palavra)}')
-        print(f'Letras tentadas: {trys}')
-        print(f"{'=' * 30}")
-        print("('_')")
-        print(letras())
-    elif points == 0:
-        print(f'Número de vidas: {points}')
-        print(f'Número de palavras: {len(palavra)}')
-        print(f'Letras tentadas: {trys}')
-        print(f"{'=' * 30}")
+    print("=" * 20)
+    for c in range(0, life):
+        print(' ' * 6, boneco[c])
+    if life == 0:
         print("(X_X)")
-        print(letras())
-        print(f"{'=' * 30}")
 
+    print(f"Vida: {life}", end=' ' * 5)
+    print("Palavra: ", end='')
+    for c in oculta:
+        print(c, end=' ')
+    print("Suas tentativas: ", trys)
+    print()
